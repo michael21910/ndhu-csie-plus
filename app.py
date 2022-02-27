2d9dcbc7a4764930ccd11255515f83d8e2808d5b
keywind127 <68499290+keywind127@users.noreply.github.com>
2022-02-27 11:22:17 +0800

    [ 2022/02/27 ]

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder = 'templates')
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", question_list = ["hello world 1", "hello world 2"])
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = False, port = 1234)
