6bcd2b51caf77bbaa89e646fdf8275a9d4d427d8
keywind127 <68499290+keywind127@users.noreply.github.com>
2022-02-27 13:46:56 +0800

    [ 2022/02/27 ]

--- app.py
+++ app.py
@@ -6,21 +6,23 @@ app = Flask(__name__, template_folder = 'templates')
 db = databaseUtils(
     '127.0.0.1',
     'root',
-    'root',
+    '',
     'csieplus'
 )
 
 connection = db.connect(
     '127.0.0.1',
     'root',
-    'root',
+    '',
     'csieplus'
 )
 
 @app.route('/')
 def index():
     question_list = db.get_index_contents(connection)
-    return render_template("index.html", question_list = question_list)
+    print(question_list)
+    username = "Wilson"
+    return render_template("index.html", question_list = question_list, username = username)
 
 if __name__ == '__main__':
-    app.run(debug = False, port = 1234)
+    app.run(debug = False)
--- databaseUtils.py
+++ databaseUtils.py
@@ -1,4 +1,18 @@
 import pymysql.cursors
+import copy
+
+def format_string(question_list):
+    _q = copy.deepcopy(question_list)
+    for i, q in enumerate(question_list):
+        for key, val in q.items():
+            if (key == "replies"):
+                _q[i][key] = "{} {}".format(val, "replies" if (val > 1) else "reply")
+            elif (key == "time"):
+                _q[i][key] = val.strftime("%Y/%m/%d")
+            elif (key == "id"):
+                _q[i][key] = "question-{}".format(val)
+        del _q[i]["id"]
+    return _q
 
 class databaseUtils:
     def __init__(self, host, username, password, database):
@@ -28,8 +42,12 @@ class databaseUtils:
                 sql = "SELECT * FROM `questions`"
                 cursor.execute(sql)
                 result = cursor.fetchall()
+                """
                 result = tuple(tuple(row.values()) for row in result)
+                """
+                result = format_string(result)
                 return result
         except Exception as e:
             print(e)
             return 'False'
+    
