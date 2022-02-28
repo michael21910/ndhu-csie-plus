9b0f2c904bb50632ea8980b3fb6105c1c8e0cb85
keywind127 <68499290+keywind127@users.noreply.github.com>
2022-02-28 21:58:59 +0800

    [ 2022/02/28 ] [ Added Functionality ]
    1. Added "upvote/like" functionality 
    2. Limited "ask question", "add reply", "like question" to logged-in users

--- app.py
+++ app.py
@@ -17,54 +17,57 @@ connection = db.connect(
     "csieplus"
 )
 
-username = "user000"
+username = ""
 
 @app.route("/")
 def index():
     question_list = db.get_index_contents(connection)
     # a test to see if the jinja if statement works
-    username = "User001"
-    return render_template("index.html", question_list = question_list, username = username)
+    return render_template("index.html", question_list = ([] if (question_list == None) else question_list), username = username)
 
-@app.route("/ask")
-def ask():
-    username = "User101"
+@app.route("/ask_question")
+def ask_question():
     return render_template("ask.html", username = username)
 
-@app.route("/post", methods = ["POST"])
+@app.route("/post_question", methods = ["POST"])
 def post_question():
-    #question_list = db.get_index_contents(connection)
-    #return render_template("index.html", question_list = question_list, username = "success")
-    #print("\n\nreceived\n\n")
-    question_dict = request.get_json()
-    question_dict["asker"] = ("" if (question_dict["anonymous"]) else username)
-    err = db.insert_question(connection, question_dict)
     
-    #print("\n\n")
-    #print(question_dict)
-    #print("\n\n")
+    if (username == ""):
+        err = 2
+    else:
+        question_dict = request.get_json()
+        question_dict["asker"] = ("" if (question_dict["anonymous"]) else username)
+        err = db.insert_question(connection, question_dict)
     
-    return jsonify({ "error_code" : "success" } if (err == 1) else { "error_code" : "failure" })
+    return jsonify({ "error_code" : "success" } if (err == 1) else { "error_code" : "failure" } if (err == -1) else { "error_code" : "please login first" })
 
-@app.route("/question/<question_id>", methods = ["GET", "POST"])
-def redirect_question(question_id):
-    print(question_id)
+@app.route("/question", methods = ["GET"])
+def redirect_question():
+    question_id = int(request.args.get("qid"))
     question_contents = db.get_question_contents(connection, question_id)
-    print(question_contents)
     if (question_contents == None):
         render_template("404.html")
     else:
-        return render_template("question.html", username = "hello", question_contents = question_contents)
+        return render_template("question.html", username = username, question_contents = question_contents)
 
-@app.route("/post_reply", methods = ["GET", "POST"])
+@app.route("/post_reply", methods = ["POST"])
 def post_reply():
+    if (username == ""):
+        err = 2
+    else:
+        reply_contents = request.get_json()
+        reply_contents["replier"] = username
+        err = db.insert_reply(connection, reply_contents)
+    return jsonify({ "error_code" : "success" } if (err == 1) else { "error_code" : "failure" } if (err == -1) else { "error_code" : "please login first" })
+      
+@app.route("/upvote_question", methods = ["POST"])
+def upvote_question():
+    if (username == ""):
+        err = 2
+    else:
+        contents = request.get_json()
+        err = db.upvote_question(connection, contents["question_id"], username)
+    return jsonify({ "error_code" : "success" } if (err == 1) else { "error_code" : "failure" } if (err == -1) else { "error_code" : "please login first" })
     
-    username = "User101"
-    reply_contents = request.get_json()
-    reply_contents["replier"] = username
-    err = db.insert_reply(connection, reply_contents)
-    print(reply_contents)
-    return jsonify({ "error_code" : "success" } if (err == 1) else { "error_code" : "failure" })
-        
 if __name__ == "__main__":
     app.run(debug = False, port = 1234)
