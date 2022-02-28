1264a2ce127fd20aaeebcb748d9e5f43df9c7432
michael21910 <faefae31910@gmail.com>
2022-02-28 23:36:35 +0800

    [ 2022/02/28 ] [ login system finished ]
    3 functions added:
    - login system
    - register system
    - forgot password system

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from databaseUtils import databaseUtils
 
 app = Flask(__name__, template_folder = "templates")
@@ -17,12 +17,78 @@ connection = db.connect(
     "csieplus"
 )
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def index():
+    global question_list
+    global username
+    global password
     question_list = db.get_index_contents(connection)
-    # a test to see if the jinja if statement works
-    username = "User001"
-    return render_template("index.html", question_list = question_list, username = username)
+    if request.method == "POST":
+        username = request.values["username"]
+        password = request.values["password"]
+        if db.login_user_check(connection, username, password) == "True":
+            print("usename:", username)
+            return render_template("index.html", question_list = question_list, username = username)
+        elif db.login_user_check(connection, username, password) == "DNE":
+            return render_template("login.html", message = "Username or password is incorrect.")
+        elif db.login_user_check(connection, username, password) == "False":
+            return redirect(url_for("FOF"))
+    else:
+        return render_template("index.html", question_list = question_list, username = "")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", message = "")
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.values["username"]
+        email = request.values["email"]
+        password = request.values["password"]
+        repeatPassword = request.values["repeat-password"]
+        if username == "" or email == "" or password == "" or repeatPassword == "":
+            return render_template("register.html", message = "Please fill in all fields.")
+        else:
+            if db.check_username_exists(connection, username) == "True":
+                render_template("register.html", message = "Username already exists.")
+            elif db.check_username_exists(connection, username) == "DNE":
+                if password == repeatPassword:
+                    db.add_user(connection, username, email, password)
+                    return render_template("login.html", message = "You have successfully registered.")
+                else:
+                    return render_template("register.html", message = "Password is not the same.")
+            elif db.check_username_exists(connection, username) == "False":
+                return redirect(url_for("FOF"))
+    return render_template("register.html", message = "")
+
+@app.route("/forgotpassword", methods = ["GET", "POST"])
+def forgotpassword():
+    if request.method == "POST":
+        print("POSTTTTT")
+        username = request.values["username"]
+        token = request.values["token"]
+        newPassword = request.values["new-password"]
+        repeatNewPassword = request.values["repeat-new-password"]
+        print(username, token, newPassword, repeatNewPassword)
+        if username == "" or token == "" or newPassword == "" or repeatNewPassword == "":
+            return render_template("forgotpassword.html", message = "Please fill in all fields.")
+        else:
+            if db.change_password_user_check(connection, username, token) == "True":
+                if newPassword == repeatNewPassword:
+                    db.update_password(connection, username, newPassword)
+                    return render_template("login.html", message = "You have successfully changed your password.")
+                else:
+                    return render_template("forgotpassword.html", message = "Password is not the same.")
+            elif db.change_password_user_check(connection, username, token) == "DNE":
+                return render_template("forgotpassword.html", message = "Username or token is incorrect.")
+            elif db.change_password_user_check(connection, username, token) == "False":
+                return redirect(url_for("FOF"))
+    return render_template("forgotpassword.html", message = "")
+
+@app.route("/404")
+def FOF():
+    return render_template("404.html")
 
 if __name__ == "__main__":
-    app.run(debug = False)
+    app.run(debug = True, port = 5000)
--- databaseUtils.py
+++ databaseUtils.py
@@ -1,5 +1,10 @@
 import pymysql.cursors
 import copy
+import string
+import random
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
 class databaseUtils:
     def __init__(self, host, username, password, database):
@@ -52,5 +57,93 @@ class databaseUtils:
                 elif key == "id":
                     modified_question_list[qid][key] = \
                         "question-{}".format(val)
-        print(modified_question_list)
         return modified_question_list
+
+    def login_user_check(self, connection, username, password):
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `users` WHERE `username` = %s AND `password` = %s"
+                cursor.execute(sql, (username, password))
+                result = cursor.fetchone()
+                if result is not None:
+                    return "True"
+                else:
+                    return "DNE"
+        except Exception as e:
+            print(e)
+            return "False"
+
+    def check_username_exists(self, connection, username):
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `users` WHERE `username` = %s"
+                cursor.execute(sql, (username, ))
+                result = cursor.fetchone()
+                if result is not None:
+                    return "True"
+                else:
+                    return "DNE"
+        except Exception as e:
+            print(e)
+            return "False"
+
+    def add_user(self, connection, username, email, password):
+        try:
+            with connection.cursor() as cursor:
+                token = ''.join(random.sample(string.ascii_letters + string.digits, 10))
+                sql = "INSERT INTO `users` (`username`, `email`, `password`, `token`) VALUES (%s, %s, %s, %s)"
+                cursor.execute(sql, (username, email, password, token))
+                connection.commit()
+                databaseUtils.send_email(email, token)
+                return "True"
+        except Exception as e:
+            print(e)
+            return "False"
+
+    def send_email(email, token):
+        content = MIMEMultipart()
+        content["subject"] = "Welcome to NDHU CSIEplus"
+        content["from"] = "ndhucsieplus@gmail.com"
+        content["to"] = email
+        content.attach(MIMEText("""
+Hello,
+Welcome to NDHU CSIEplus!
+Your token is: """ + token + """,
+This token is needed to change the password, please keep it safe.
+            """))
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            try:
+                server.ehlo()
+                server.login("ndhucsieplus@gmail.com", "xhheytxfagcimsqa")
+                server.send_message(content)
+                print("email send to: ", str(email))
+            except Exception as e:
+                print(e)
+
+    def change_password_user_check(self, connection, username, token):
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `users` WHERE `username` = %s AND `token` = %s"
+                cursor.execute(sql, (username, token))
+                result = cursor.fetchone()
+                print(result)
+                if result is not None:
+                    print("change_password_user_check TRUE")
+                    return "True"
+                else:
+                    print("change_password_user_check DNE")
+                    return "DNE"
+        except Exception as e:
+            print(e)
+            return "False"
+
+    def update_password(self, connection, username, password):
+        try:
+            with connection.cursor() as cursor:
+                sql = "UPDATE `users` SET password = %s WHERE username = %s"
+                cursor.execute(sql, (password, username))
+                connection.commit()
+                return "True"
+        except Exception as e:
+            print(e)
+            return "False"
