3e6c24083dbd3c36c79c6aaf058f5863373ed419
keywind127 <68499290+keywind127@users.noreply.github.com>
2022-02-28 17:39:47 +0800

    [ 2022/02/28 ] [ Added Several Pages ]

--- app.py
+++ app.py
@@ -1,25 +1,70 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from databaseUtils import databaseUtils
 
-app = Flask(__name__, template_folder = 'templates')
+app = Flask(__name__, template_folder = "templates")
 
 db = databaseUtils(
-    '127.0.0.1',
-    'root',
-    'root',
-    'csieplus'
+    "127.0.0.1",
+    "root",
+    "",
+    "csieplus"
 )
 
 connection = db.connect(
-    '127.0.0.1',
-    'root',
-    'root',
-    'csieplus'
+    "127.0.0.1",
+    "root",
+    "",
+    "csieplus"
 )
 
-@app.route('/')
-def index():
-    return render_template("index.html", question_list = ["hello world 1", "hello world 2"])
+username = "user000"
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    question_list = db.get_index_contents(connection)
+    # a test to see if the jinja if statement works
+    username = "User001"
+    return render_template("index.html", question_list = question_list, username = username)
+
+@app.route("/ask")
+def ask():
+    username = "User101"
+    return render_template("ask.html", username = username)
+
+@app.route("/post", methods = ["POST"])
+def post_question():
+    #question_list = db.get_index_contents(connection)
+    #return render_template("index.html", question_list = question_list, username = "success")
+    #print("\n\nreceived\n\n")
+    question_dict = request.get_json()
+    question_dict["asker"] = ("" if (question_dict["anonymous"]) else username)
+    err = db.insert_question(connection, question_dict)
+    
+    #print("\n\n")
+    #print(question_dict)
+    #print("\n\n")
+    
+    return jsonify({ "error_code" : "success" } if (err == 1) else { "error_code" : "failure" })
+
+@app.route("/question/<question_id>", methods = ["GET", "POST"])
+def redirect_question(question_id):
+    print(question_id)
+    question_contents = db.get_question_contents(connection, question_id)
+    print(question_contents)
+    if (question_contents == None):
+        render_template("404.html")
+    else:
+        return render_template("question.html", username = "hello", question_contents = question_contents)
+
+@app.route("/post_reply", methods = ["GET", "POST"])
+def post_reply():
+    
+    username = "User101"
+    reply_contents = request.get_json()
+    reply_contents["replier"] = username
+    err = db.insert_reply(connection, reply_contents)
+    print(reply_contents)
+    return jsonify({ "error_code" : "success" } if (err == 1) else { "error_code" : "failure" })
+        
+if __name__ == "__main__":
     app.run(debug = False, port = 1234)
